98e4c6736b3be562c1696e59552914e7a488aadf
Art Beatte IV <abeatte@gmail.com>
2022-12-12 21:27:36 -0800

    HACK: Ensures that paint pallet is only 5 across (otherwise phones will scroll

--- src/components/PaintPalette.tsx
+++ src/components/PaintPalette.tsx
@@ -154,8 +154,12 @@ const PaintPalette = () => {
                 <div className="Items_row">
                     {tiles}
                 </div>
+                {/* TODO: fix this with styling, not a slice */}
                 <div className="Items_row">
-                    {[kill, ...characters]}
+                    {[kill, ...characters.slice(0, 4)]}
+                </div>
+                <div className="Items_row">
+                    {characters.slice(4)}
                 </div>
                 <div className="Items_row">
                     {items}
