af7b42575dfb2497da4f7858034e146cf15ae004
Art Beatte IV <abeatte@gmail.com>
2024-10-05 22:15:04 -0700

    Restricts two-panel title screen to greater than 674px in width.

--- src/components/TitleScreen.tsx
+++ src/components/TitleScreen.tsx
@@ -17,6 +17,8 @@ import { Type as TileType } from '../models/TileModel';
 
 const logo = require('../images/icon.png');
 
+const MIN_TWO_PANEL_WIDTH = 675;
+
 class TitleScreen extends React.Component<AppState & DispatchProps> {
     keysFunction = (event: { key: string; }) => {
         const key = event.key.toUpperCase();
@@ -41,11 +43,13 @@ class TitleScreen extends React.Component<AppState & DispatchProps> {
         const selected_paint = this.props.paintPalette.selected;
         const selected_position = this.props.board.selected_position;
 
+        const show_map_preview = this.props.board.view_port.width_measurement > MIN_TWO_PANEL_WIDTH;
+
         return (
             <div>
-                <Dashboard showPause={false} showGameStats={false} showStats={false} onReset={this.props.resetBoard} />
+                {show_map_preview && <Dashboard showPause={false} showGameStats={false} showStats={false} onReset={this.props.resetBoard} />}
                 <div className="TitleContainer">
-                    <div className="TitleSection">
+                    <div className="TitleSection" style={{ width: show_map_preview ? "50%" : "100%" }}>
                         <h1>Welcome to</h1>
                         <img className="Logo" src={logo} alt='logo' />
                         <h3>How will you be playing?</h3>
@@ -64,29 +68,31 @@ class TitleScreen extends React.Component<AppState & DispatchProps> {
                             </button>
                         </div>
                     </div>
-                    <div className="TitleSection">
-                        <div className="MapContainer">
-                            <Map
-                                view_port={{ ...this.props.board.view_port, height: this.props.board.arena.height }}
-                                purpose={Purpose.Map}
-                                selectedPosition={selected_position}
-                                onTileClick={(position: number, contains_combatant: boolean) => {
-                                    if (selected_paint !== Pointer.Target) {
-                                        Analytics.logEvent('tap_on_board: Paint');
-                                        this.props.paintOnTile({ position, type: selected_paint });
-                                    }
-                                }}
-                                onTileDragEnter={(position: number) => {
-                                    Analytics.logEvent('drag_on_board');
-                                    if (Object.keys(TileType).includes(selected_paint)) {
-                                        this.props.paintOnTile({ position, type: selected_paint });
-                                    }
-                                }}
-                            />
+                    {show_map_preview &&
+                        <div className="TitleSection">
+                            <div className="MapContainer">
+                                <Map
+                                    view_port={{ ...this.props.board.view_port, height: this.props.board.arena.height }}
+                                    purpose={Purpose.Map}
+                                    selectedPosition={selected_position}
+                                    onTileClick={(position: number, contains_combatant: boolean) => {
+                                        if (selected_paint !== Pointer.Target) {
+                                            Analytics.logEvent('tap_on_board: Paint');
+                                            this.props.paintOnTile({ position, type: selected_paint });
+                                        }
+                                    }}
+                                    onTileDragEnter={(position: number) => {
+                                        Analytics.logEvent('drag_on_board');
+                                        if (Object.keys(TileType).includes(selected_paint)) {
+                                            this.props.paintOnTile({ position, type: selected_paint });
+                                        }
+                                    }}
+                                />
+                            </div>
                         </div>
-                    </div>
+                    }
                 </div>
-                <PaintPalette showTarget={false} />
+                {show_map_preview && <PaintPalette showTarget={false} />}
             </div>
         );
     }
